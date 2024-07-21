70ebfc9fe9b39014ddb77a44512ece4f96291621
rohitpardhi1998 <rohitpardhi90@gmail.com>
2024-07-21 21:31:51 +0530

    added comment in routes

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -5,14 +5,14 @@ import { Switch, Route } from 'react-router-dom';
 // Components
 import Home from '../pages/Home/Home';
 import Cart from '../pages/Cart/Cart';
-import Checkout from '../pages/Checkout/Checkout';
-
+// import Checkout from '../pages/Checkout/Checkout';
+///test impot 
 const Routes = () => {
   return (
   <Switch>
     <Route path="/" component={ Home } exact />    
     <Route path="/cart" component={ Cart } exact />    
-    <Route path="/checkout" component={ Checkout } exact />    
+    {/* <Route path="/checkout" component={ Checkout } exact />     */}
   </Switch>
   );
 };
